3fa57a654662825b7b91f0e87c9eb8b4f267f484
Hector E. Gomez Morales <hectoregm@gmail.com>
2011-10-11 16:46:43 -0500

    Add Twitter configuration. Load config values from yaml file.

--- app.rb
+++ app.rb
@@ -1,11 +1,27 @@
 require "sinatra/base"
-require "sinatra/reloader" if development?
+require "sinatra/reloader"
 require "twitter"
 
 class Star < Sinatra::Base
 
+  configure do
+    @@config = YAML.load_file('config.yml')
+
+    Twitter.configure do |config|
+      config.consumer_key = @@config["twitter"]["consumer_key"]
+      config.consumer_secret = @@config["twitter"]["consumer_secret"]
+      config.oauth_token = @@config["twitter"]["oauth_token"]
+      config.oauth_token_secret = @@config["twitter"]["oauth_token_secret"]
+    end
+  end
+
+  configure :development do
+    register Sinatra::Reloader
+    also_reload "*.rb"
+  end
+
   get '/' do
-    'Star'
+    Twitter.favorites[0].to_s
   end
 
 end
