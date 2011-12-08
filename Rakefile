3b7ac1ba222b81483000ee2c3918e193a9ba184c
Hector E. Gomez Morales <hectoregm@gmail.com>
2011-12-08 13:47:16 -0600

    Added jst rake task to build js templates for jasmine  tests.

--- Rakefile
+++ Rakefile
@@ -7,3 +7,9 @@ rescue LoadError
     abort "Jasmine is not available. In order to run jasmine, you must: (sudo) gem install jasmine"
   end
 end
+
+desc 'Create or update jst.js template'
+task :jst do
+  templates = %x{curl -s "star-reader.dev/javascripts/jst.js"}
+  File.open('spec/javascripts/templates/jst.js', 'w') { |f| f.write(templates) } 
+end
--- spec/javascripts/templates/jst.js
+++ spec/javascripts/templates/jst.js
@@ -2,7 +2,7 @@
   var c = {};
   if (!window.JST) window.JST = {};
   JST["star-reader/stream"] = function() {
-    if (!c["star-reader/stream"]) c["star-reader/stream"] = (_.template("<nav>\n  <ul class=\"tabs\">\n    <li id=\"main\" class=\"<%= section === 'main' ? \"active\" : \"\" %>\">\n      <a href=\"/\">Home</a>\n    </li>\n    <li id=\"archives\" class=\"<%= section === 'archives' ? \"active\" : \"\" %>\">\n      <a href=\"/archives\">Archives</a>\n    </li>\n  </ul>\n</nav>\n<section class=\"star-stream\">\n</section>\n"));
+    if (!c["star-reader/stream"]) c["star-reader/stream"] = (_.template("<nav>\n  <ul class=\"tabs\">\n    <li id=\"main\" class=\"<%= section === 'main' ? \"active\" : \"inactive\" %>\">\n      <a href=\"/\">Home</a>\n    </li>\n    <li id=\"archives\" class=\"<%= section === 'archives' ? \"active\" : \"inactive\" %>\">\n      <a href=\"/archives\">Archives</a>\n    </li>\n  </ul>\n</nav>\n<section class=\"star-stream\">\n</section>\n"));
     return c["star-reader/stream"].apply(this, arguments);
   };
 })();
